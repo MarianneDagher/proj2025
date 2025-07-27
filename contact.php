4b787ff28087141ba57dc5e8dea30fb3e03155e2
MarianneDagher <166322724+MarianneDagher@users.noreply.github.com>
2025-07-27 20:02:24 +0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="we have a collection of glam products ">
+    <meta name="keywords" content="eyes,lips,face,equipments">
+    <link rel="stylesheet"href="css/styles.css">
+    <script src="javascript/script.js"> </script>
+    <title>our shop </title>
+
+    <style> footer{
+        position:fixed;
+        bottom:0;
+    }</style>
+  </head>  
+
+<body id="contact">
+ <?php include "includes/nav.php"?>
+ <?php include "includes/header.php"?>
+
+  
+  <main>
+    <h2> this is the contact page</h2>
+  </main>
+
+  
+
+<?php include "includes/footer.php"?>
+    
+
+    </body>
+    </html>
