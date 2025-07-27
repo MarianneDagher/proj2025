72fc393c17f9bf829148fcd56d126c1e0a5577d6
MarianneDagher <166322724+MarianneDagher@users.noreply.github.com>
2025-07-27 20:06:47 +0300

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+ <nav>
+  <div class="brand">BEAUTYLINE</div>
+  <div class="links">
+    <a  data-active="index" href="index.php">home<a>
+       <a  data-active="about" href="index.php">About<a>
+         <a  data-active="contacts" href="index.php">Contacts<a>
+  </div>
+</nav>
